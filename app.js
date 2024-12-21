0051b136b91fac9d20fd176f824b0f0ff9e207f6
missharma <khushisharma13122002@gmail.com>
2024-12-21 12:44:06 -0800

    Initial Setup Complete

---
+++ app.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');  // Change from 'mysql' to 'mysql2'
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',             // Replace with your MySQL username
+  password: '#MissSharma',// Replace with your MySQL password
+  database: 'JobsList' // Replace with your database name
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+    return;
+  }
+  console.log('Connected to the MySQL database.');
+});
